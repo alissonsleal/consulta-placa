a7e1986e592ea66234fa6cb5d6a40b9e9e78677c
Alisson Leal <alissonsleal@hotmail.com>
2020-12-13 20:10:29 -0300

    fixed labels for screen readers

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -22,7 +22,14 @@ export const Container = styled.div`
     flex-direction: column;
 
     label {
-      display: none;
+      position: absolute;
+      overflow: hidden;
+      clip: rect(0 0 0 0);
+      height: 1px;
+      width: 1px;
+      margin: -1px;
+      padding: 0;
+      border: 0;
     }
     input {
       background: var(--light-background);
